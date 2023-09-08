b657558503d8845d41e31f0344e330d9111d7193
ndrada <andrada.groseanu@gmail.com>
2023-09-08 13:41:58 -0700

    calculator-project

--- script.js
+++ script.js
@@ -64,3 +64,22 @@ function calculateTime(){
 
     setTimeout(calculateTime, 200);
 }   
+
+// calculator
+const userInput = document.getElementById('calcUserInput');
+let expression = '';
+
+function press(num){
+    expression += num;
+    userInput.value = expression;
+}
+
+function equal(){
+    userInput.value = eval(expression); 
+    expression = '';
+}
+
+function erase(){
+    expression = '';
+    userInput.value = expression;
+}
